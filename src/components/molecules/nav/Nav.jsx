fecae5e1fa2b76b3308b567051e68fa3a44fcf23
joppe montezinos <joppe.montezinos@gmail.com>
2023-08-31 13:07:01 +0200

    [maestro-joppe] added components plus basic styling that can be changed

---
+++ src/components/molecules/nav/Nav.jsx
@@ -0,0 +1,12 @@
+import Form from '../form/Form';
+import styles from './Nav.module.scss';
+
+const Nav = () => {
+  return (
+    <div className={styles.loginContainer}>
+      <Form />
+    </div>
+  );
+};
+
+export default Nav;
