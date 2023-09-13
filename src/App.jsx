4f75121c19b96ac3cfec0438067d451becc023b7
joppe montezinos <joppe.montezinos@gmail.com>
2023-09-13 14:02:47 +0200

    [kage] fixing errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import './App.scss';
-import mainNav from './components/pages/Main Menu/menu';
-//import Login from './components/organisms/login/Login';
+import Login from './components/organisms/login/Login';
 
 const App = () => {
 
   return (
     <div id="App">
     {/* add if statement to check for user login */}
-      <MainNav />
+      <Login />
     </div>
   );
 };
