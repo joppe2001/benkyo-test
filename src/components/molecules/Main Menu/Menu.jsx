96dac36a0716c333d9c7330fb8be04238b10bc8c
KKaged <dhigaredaleon707@gmail.com>
2023-09-13 18:37:33 +0900

    Started on main menu page.

---
+++ src/components/molecules/Main Menu/Menu.jsx
@@ -0,0 +1,16 @@
+import styled from "@emotion/styled"
+
+const mainNav = () => {
+    return(
+    <div className={styles.navContainer}>
+        <div className={styles.leftSide}>
+            <p>Home</p>
+            <p>Communities</p>
+            <p>Courses</p>
+            <p>Contact Us</p>
+        </div>
+        <div className={styles.rightSide}></div>
+    </div>)
+}
+
+export default mainNav;
