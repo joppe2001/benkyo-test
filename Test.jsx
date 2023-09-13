1c5ee85ba89a1447765444537cbf4484458b1c81
joppe montezinos <joppe.montezinos@gmail.com>
2023-09-13 14:14:30 +0200

    [kage] fixing errors

---
+++ Test.jsx
@@ -0,0 +1,14 @@
+import './App.scss';
+import Login from './components/organisms/login/Login';
+
+const App = () => {
+
+  return (
+    <div id="App">
+    {/* add if statement to check for user login */}
+      <Login />
+    </div>
+  );
+};
+
+export default App;
