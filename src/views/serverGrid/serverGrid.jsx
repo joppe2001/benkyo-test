1b7d9f865df1ed9947c95bc89bbc53fdfb5c0f31
joppe montezinos <joppe.montezinos@gmail.com>
2023-09-18 00:06:19 +0200

    [maestro-joppe] adding the grid menu things

--- src/views/serverGrid/serverGrid.jsx
+++ src/views/serverGrid/serverGrid.jsx
@@ -1,10 +1,36 @@
+import React from "react";
+import styles from "./serverGrid.module.scss";
+import Menu from "../../components/molecules/MainMenu/Menu";
+
 const ServerGrid = () => {
-    // this is a page where all the chatting serrvers will be displayed in a grid format
-    return (
-        <div>
-            <h1>Server Grid</h1>
-        </div>
-    );
-}
+	// Dummy data to represent the servers you'll fetch from Firebase
+	const servers = [
+		{ id: "1", name: "Server 1" },
+		{ id: "2", name: "Server 2" },
+		{ id: "3", name: "Server 3" },
+		{ id: "4", name: "Server 4" },
+		{ id: "5", name: "Server 5" },
+		{ id: "6", name: "Server 6" },
+		{ id: "7", name: "Server 7" },
+		{ id: "8", name: "Server 8" },
+		{ id: "9", name: "Server 9" },
+	];
+
+	return (
+		<div className={styles.outerContainer}>
+            <Menu />
+			<div className={styles.serverGridContainer}>
+				<div className={styles.grid}>
+					<div className={styles.gridItemTitle}>Center Title</div>{" "}
+					{servers.map((server) => (
+						<div key={server.id} className={styles.gridItem}>
+							{server.name}
+						</div>
+					))}
+				</div>
+			</div>
+		</div>
+	);
+};
 
 export default ServerGrid;
