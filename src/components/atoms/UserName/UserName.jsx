bc72bccf79b502fa5c128d441c33f282f27c9fd0
joppe montezinos <joppe.montezinos@gmail.com>
2023-09-27 22:47:21 +0200

    [main-test] adding a bunch of styling changes

--- src/components/atoms/UserName/UserName.jsx
+++ src/components/atoms/UserName/UserName.jsx
@@ -1,34 +1,10 @@
-import React, { useEffect, useState } from 'react';
 import { useAuthState } from '../../../store/authState';
-import { getUser } from '../../../firebase/db';
 import styles from './UserName.module.scss';
 
 const UserDisplayName = ({ onLogout }) => {
-  const { user, isLoggedIn } = useAuthState();
-  const [userName, setUserName] = useState('');
-
-  useEffect(() => {
-    if (!isLoggedIn) return;
-
-    const fetchUserInfo = async () => {
-      try {
-        const userInfo = await getUser(user.uid);
-        if (userInfo && userInfo.displayName) {
-          setUserName(userInfo.displayName);
-        } else {
-          console.error(
-            "User doesn't have a displayName or userInfo is undefined"
-          );
-        }
-      } catch (error) {
-        console.error('Error fetching user info:', error);
-      }
-    };
-
-    fetchUserInfo();
-  }, [isLoggedIn, user]);
-
-  if (!isLoggedIn) return <span></span>;
+  const { isLoggedIn } = useAuthState();
+  
+  if (!isLoggedIn) return <span>Please log in.</span>;
 
   return (
     <div className={styles.userNameContainer}>
